70cf925e03e6e0179359ee7551961a89f07dd0b8
aleksandertap <aleksander.tapsi@voco.ee>
2025-04-01 10:45:36 +0300

    yl5 EUR currency added

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,20 +1,23 @@
 const MealItem = (props) => {
-    return (
-        <li className="meal-item">
-            <article>
-                <img src={require(`../assets/${props.meals.image}`)} alt={props.meals.name}/>
-                <div>
-                    <h3>{props.meals.name}</h3>
-                    <p className="meal-item-price">{props.meals.price}</p>
-                    <p className="meal-item-description ">{props.meals.description}</p>
-                </div>
-                <p>
-                    <button className="meal-item-actions">Add to Cart</button>
-                </p>
-            </article>
-        </li>
-        
-    )
-}
+  const price = props.meals.price;
 
-export default MealItem
+  return (
+    <li className="meal-item">
+      <article>
+        <img src={require(`../assets/${props.meals.image}`)} alt={props.meals.name} />
+        <div>
+          <h3>{props.meals.name}</h3>
+          <p className="meal-item-price">
+            {new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(price)}
+          </p>
+          <p className="meal-item-description ">{props.meals.description}</p>
+        </div>
+        <p>
+          <button className="meal-item-actions">Add to Cart</button>
+        </p>
+      </article>
+    </li>
+  );
+};
+
+export default MealItem;
