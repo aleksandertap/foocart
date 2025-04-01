71284a050bb67a8e43622cb056f0b487529d1692
aleksandertap <aleksander.tapsi@voco.ee>
2025-04-01 17:06:16 +0300

    yl 9 ja 10 koos. lisasin arvutused, kuid ei suuda kuvada dialoogi ega close ja checkout nupud ei tee ka midagi . sad

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Headers from "./components/Header.js";
 import Meals from "./components/Meals.js";
 import { useState, useEffect,} from "react";
 import CartProvider from "./store/CartContext.js";
+import Modal from "./components/UI/Modal.js"
 
 
 const App = () => {
@@ -18,7 +19,9 @@ const App = () => {
   }, []);
   return (
       <>
+      
       <CartProvider >
+      <Modal/>
       <Headers />
       <Meals meals = {meals}/>
       </CartProvider>
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,10 +1,42 @@
-const Modal = (props) => {
-    return ( 
-        <dialog>
-            {props.children}
-        </dialog>
-        
-    )
-}
+import { createPortal } from "react-dom";
+import { CartContext } from "../../store/CartContext";
+import { useContext } from "react";
+import Button from "./Button";
 
-export default Modal
+const ModalOverlay = () => {
+  const { cart } = useContext(CartContext);
+
+  return (
+    <dialog className="modal cart">
+      <h2>Your cart</h2>
+      {
+        <ul>
+          {cart.map((meal, index) => {
+            return (
+              <li className="cart-item" key={index}>
+                <p>
+                  {meal.name} - {meal.quantity}
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+      }
+      <p className="cart-total">
+        {new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(
+          cart.reduce((sum, meal) => sum + meal.price * meal.quantity, 0)
+        )}
+      </p>
+      <p>
+        <Button textOnly={true}>Close</Button>
+        <Button>Checkout</Button>
+      </p>
+    </dialog>
+  );
+};
+
+const Modal = (props) => {
+  return createPortal(<ModalOverlay />, document.getElementById("modal"));
+};
+
+export default Modal;
